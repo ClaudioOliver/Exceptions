8a7c964ad2bb3e1ca064e89b30eaa02691b3fdbe
ClaudioOliver <claudio.oliveira127@outlook.com>
2024-04-16 19:04:18 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+        try {
+            System.out.println("Digite um valor: ");
+            int numero1 = s.nextInt();
+
+            System.out.println(numero1);
+
+        } catch (Exception ex) {
+            // TODO: handle exception
+            System.out.println("Erro: Valor não é um número");
+        }
+
+    }
+}
